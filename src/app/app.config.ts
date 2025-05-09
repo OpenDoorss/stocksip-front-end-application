e8b1e6a079c3c9f0c1d78391cb2df92a818d1889
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-05-09 13:48:13 -0500

    feat(i18n): add i18n support.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {TranslateModule} from '@ngx-translate/core';
+
+export const httpLoaderFactory = (http: HttpClient) => new TranslateHttpLoader(http);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+
+    /**
+     * i18n Support
+     */
+    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot({
+        loader: {
+          provide: TranslateHttpLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient]
+        }
+      }
+    ))
+    ]
 };
