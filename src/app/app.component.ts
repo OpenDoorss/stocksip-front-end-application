38c7c86e0cb794ebcbc6829ac9acc189f2b7883a
Camila Huamani <jyb990106@gmail.com>
2025-05-09 21:03:47 -0500

    feat(login): fix password label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { LoginComponent} from './authentication/login/login.component';
-import { RegisterComponent} from './authentication/register/register.component';
+import {RouterOutlet} from '@angular/router';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RegisterComponent],
-  template: `<app-register></app-register>`
+  imports: [
+    RouterOutlet
+  ],
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {}
--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,27 +1,31 @@
 import { Component } from '@angular/core';
-import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
-import {MatOption} from '@angular/material/core';
-import {MatSelect} from '@angular/material/select';
-import {MatButton, MatIconButton} from '@angular/material/button';
-import {MatIcon} from '@angular/material/icon';
+import {MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {Router} from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   imports: [
-    MatFormField,
-    MatOption,
-    MatLabel,
-    MatSelect,
-    MatButton,
-    MatInput,
-    MatIconButton,
-    MatIcon
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule
   ],
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   hide = true;
   hideConfirm = true;
-  selectedRole: string = 'licoreria';
+
+  constructor(private router: Router) {}
+  goToSignIn() {
+    this.router.navigate(['/login']);
+  }
 }
