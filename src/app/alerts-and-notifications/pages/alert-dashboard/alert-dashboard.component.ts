3b5f7e99b1640fb45fa47aff58895b77756172d5
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-05-10 17:03:31 -0500

    chore: initial commit.

---
+++ src/app/alerts-and-notifications/pages/alert-dashboard/alert-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-dashboard',
+  imports: [],
+  templateUrl: './alert-dashboard.component.html',
+  styleUrl: './alert-dashboard.component.css'
+})
+export class AlertDashboardComponent {
+
+}
