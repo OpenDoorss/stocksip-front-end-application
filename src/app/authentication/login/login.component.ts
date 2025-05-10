e1ba72711c97c0ac3128960c6f7ca0a342040bfa
Camila Huamani <jyb990106@gmail.com>
2025-05-09 20:10:06 -0500

    feat(login): add login component

---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule
+  ]
+})
+export class LoginComponent {
+  hide = true;
+}
