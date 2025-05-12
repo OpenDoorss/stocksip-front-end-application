a033d921462109112637724e2bd22537bf78cd2b
Farid Coronel <faridce14@gmail.com>
2025-05-12 06:14:59 -0500

    feat(warehouses): add id in warehouse path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from '@angular/router';
 const WarehouseComponent  = () => import('./inventory-management/pages/warehouses/warehouses.component').then(m => m.WarehousesComponent);
 
 export const routes: Routes = [
-  { path: 'warehouse', loadComponent: WarehouseComponent }
+  { path: 'warehouse/:profileId', loadComponent: WarehouseComponent }
 ];
