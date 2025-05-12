a521ade8aeaeca241e8aa9e8631669559e466e21
Farid Coronel <faridce14@gmail.com>
2025-05-12 02:01:56 -0500

    feat(werehouses): add warehouse entity.

---
+++ src/app/inventory-management/model/warehouse.entity.spec.ts
@@ -0,0 +1,7 @@
+import { WarehouseEntity } from './warehouse.entity';
+
+describe('WarehouseEntity', () => {
+  it('should create an instance', () => {
+    expect(new WarehouseEntity()).toBeTruthy();
+  });
+});
---
+++ src/app/inventory-management/model/warehouse.entity.ts
@@ -0,0 +1,15 @@
+export class Warehouse {
+  id: number;
+  name: string;
+  image: string;
+  location: string;
+  capacity: number;
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.image = '';
+    this.location = '';
+    this.capacity = 0;
+  }
+}
