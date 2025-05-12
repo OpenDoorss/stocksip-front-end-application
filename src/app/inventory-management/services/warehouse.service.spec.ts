d429a0f0f438b82498183b319d230f73623a3c2f
Farid Coronel <faridce14@gmail.com>
2025-05-12 04:40:28 -0500

    feat(warehouses): add warehouse service.

---
+++ src/app/inventory-management/services/warehouse.service.spec.ts
@@ -0,0 +1,7 @@
+import { WarehouseService } from './warehouse.service';
+
+describe('WarehouseService', () => {
+  it('should create an instance', () => {
+    expect(new WarehouseService()).toBeTruthy();
+  });
+});
