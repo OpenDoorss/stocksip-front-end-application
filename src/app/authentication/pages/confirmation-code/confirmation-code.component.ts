2618e552e0fb9d1714600992952e250c2e503a9f
Camila Huamani <jyb990106@gmail.com>
2025-05-13 00:02:26 -0500

    feat(confirmation-code): add confirmation-code section

--- src/app/authentication/pages/confirmation-code/confirmation-code.component.ts
+++ src/app/authentication/pages/confirmation-code/confirmation-code.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-confirmation-code',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './confirmation-code.component.html',
-  styleUrl: './confirmation-code.component.css'
+  styleUrls: ['./confirmation-code.component.css']
 })
 export class ConfirmationCodeComponent {
+  code: string[] = ['', '', '', '', '', ''];
+  codeArray = new Array(6);
+
+  autoFocusNext(event: any, index: number) {
+    const input = event.target;
+    let value = input.value.toUpperCase();
+
+    // Reemplaza cualquier carácter que no sea letra o número
+    if (!/^[A-Z0-9]$/.test(value)) {
+      input.value = '';
+      this.code[index] = '';
+      return;
+    }
+
+    this.code[index] = value;
+    input.value = value;
+
+    // Enfocar siguiente input
+    if (index < 5) {
+      input.nextElementSibling?.focus();
+    }
+  }
 
 }
