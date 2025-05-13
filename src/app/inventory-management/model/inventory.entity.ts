22a382a1611e20472a1b83615986c944174c3d01
Nicolas Juarez <juarezleonn2000@gmail.com>
2025-05-12 22:50:26 -0500

    feat(product): add inventory, product and zone entities.

--- src/app/inventory-management/model/inventory.entity.ts
+++ src/app/inventory-management/model/inventory.entity.ts
@@ -1,2 +1,41 @@
+import { Zone } from './zone.entity';
+import {Product} from './product.entity';
+import {v4 as uuid } from 'uuid';
+
+const AlertsLevel: string[] = ["Critical", "Risky", "Normal"]
+
+/**
+ * Represents an Inventory in the system.
+ */
 export class Inventory {
+  id: string;
+  zone: Zone;
+  totalQuantity: number;
+  alertLevel: string;
+  products: Array<Product>;
+  profileId: string;
+
+  /**
+   * Creates a new Inventory instance.
+   * @param inventory {Inventory} - The Object that contains the data to create the Inventory instance.
+   */
+  constructor(inventory: {
+    zone: Zone;
+    totalQuantity: number;
+    products: Array<Product>;
+    profileId: string;
+  }) {
+
+    // Validation
+    if (inventory.totalQuantity < 0) {
+      throw new Error('Inventory cannot have a negative quantity of products.');
+    }
+
+    this.id = uuid();
+    this.zone = inventory.zone;
+    this.totalQuantity = inventory.totalQuantity;
+    this.alertLevel = "Normal";
+    this.products = inventory.products || [];
+    this.profileId = inventory.profileId;
+  }
 }
--- src/app/inventory-management/model/product.entity.ts
+++ src/app/inventory-management/model/product.entity.ts
@@ -1,40 +1,67 @@
+import {v4 as uuid } from 'uuid';
+import {Money} from '../../shared/model/money';
+import {DateTime} from '../../shared/model/date-time';
+
+const States: string[] = ["In-Inventory", "Bought", "Donated", "Lost", "Consumed-Internally"]
+
+/**
+ * Represents a Product in the system.
+ */
 export class Product {
-  id: number;
+  id: string;
   productInfo: {
     name: string;
-    unitPrice: number;
-    content: number;
-    expirationDate: Date;
+    unitPrice: Money;
+    content: number; // Amount of milliliters of the product.
+    expirationDate: DateTime;
   };
   productType: string;
   currentStock: number;
-  minimumStock: number;
-  state: string;
-  providerId: number;
+  minimumStock: number; // Minimum of stock that this product can have before sending an alert.
+  state: string; // State of product, it can change in future for the product movement
+  providerId: string;
 
+  /**
+   * Creates a new Product instance.
+   * @param product {Product} - The Object that contains the data to create the Product instance.
+   */
   constructor(product: {
-    id: number,
     productInfo: {
       name: string,
-      unitPrice: number,
+      unitPrice: Money,
       content: number,
-      expirationDate: Date,
+      expirationDate: DateTime,
     },
     productType: string,
     currentStock: number,
-    minimumStock: number,
     state: string,
-    providerId: number
+    minimumStock: number,
+    providerId: string
   }) {
-    this.id = product.id || 0;
+
+    // Validation
+    if (product.productInfo.content <= 0) {
+      throw new Error("Product content must be non-negative or not null.");
+    }
+
+    if (product.currentStock <= 0) {
+      throw new Error("Product current stock must be non-negative or not null.");
+    }
+
+    if (product.minimumStock <= 0) {
+      throw new Error("Product minimum stock must be non-negative or not null.");
+    }
+
+    if (!(States.includes(product.state))) {
+      throw new Error("Product state must be In-Inventory, Bought, Donated, Loose or Consumed-Internally.");
+    }
+
+    this.id = uuid();
     this.productInfo = product.productInfo || {};
     this.productType = product.productType || '';
     this.currentStock = product.currentStock || 0;
     this.minimumStock = product.minimumStock || 5;
-    this.state = product.state || 'In-Inventory';
-    this.providerId = product.providerId || 0;
+    this.state = 'In-Inventory';
+    this.providerId = product.providerId || '';
   }
-
-
-  //TO-DO: Add state indicator to product. ex.: In-Inventory, Bought, Donated, etc.
 }
