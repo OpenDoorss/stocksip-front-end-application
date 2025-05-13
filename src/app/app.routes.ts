317a0ac40443641c0106931de0dc2403d86243bb
Farid Coronel <faridce14@gmail.com>
2025-05-12 23:55:12 -0500

    feat(warehouses): add create and edit component routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 
 const WarehouseComponent  = () => import('./inventory-management/pages/warehouses/warehouses.component').then(m => m.WarehousesComponent);
+const CreateAndEditWarehouseComponent  = () => import('./inventory-management/pages/warehouse-create-and-edit/warehouse-create-and-edit.component').then(m => m.WarehouseCreateAndEditComponent);
 
 export const routes: Routes = [
-  { path: 'warehouse/:profileId', loadComponent: WarehouseComponent }
+  { path: 'warehouse/:profileId',                   loadComponent: WarehouseComponent },
+  { path: 'warehouse/:profileId/create',            loadComponent: CreateAndEditWarehouseComponent, data: {mode: 'create'} },
+  { path: 'warehouse/:profileId/edit/:warehouseId', loadComponent: CreateAndEditWarehouseComponent, data: {mode: 'edit'} },
 ];
