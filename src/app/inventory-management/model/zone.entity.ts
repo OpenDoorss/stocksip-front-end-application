e2248a4edcdf97b25b756c4cfe166f8bd3568f77
Farid Coronel <faridce14@gmail.com>
2025-05-14 18:07:12 -0500

    feat(zones): add type attribute on zones class.

--- src/app/inventory-management/model/zone.entity.ts
+++ src/app/inventory-management/model/zone.entity.ts
@@ -4,16 +4,19 @@ import {v4 as uuid } from 'uuid';
  * Represents a Zone in the system. A Zone is like an inventory that is assigned to different locations in the liquor store.
  */
 export class Zone {
-  id: string;
+  zoneId: string;
   name: string;
+  type: string;
   description: string;
 
   constructor(zone: {
     name: string,
     description: string
+    type: string,
   }) {
-    this.id = uuid();
+    this.zoneId = uuid();
     this.name = zone.name || '';
+    this.type = zone.type || '';
     this.description = zone.description || '';
   }
 }
