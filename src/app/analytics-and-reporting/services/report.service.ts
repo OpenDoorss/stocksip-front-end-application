49a8d98fac0f0fbf80a9e208ff1e68564920811f
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-05-14 09:12:37 -0500

    feat(report): add report service

--- src/app/analytics-and-reporting/services/report.service.ts
+++ src/app/analytics-and-reporting/services/report.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BaseService } from '../../shared/services/base.service';
+import { Report } from '../model/report.entity';
+import { environment } from '../../../environments/environment';
+
+const reportsResourceEndpointPath = environment.reportsEndpointPath;
 
 @Injectable({
   providedIn: 'root'
 })
-export class ReportService {
-
-  constructor() { }
+export class ReportService extends BaseService<Report> {
+  constructor() {
+    super();
+    this.resourceEndpoint = reportsResourceEndpointPath;
+  }
 }
