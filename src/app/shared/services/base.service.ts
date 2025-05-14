35ad680e860ca4cd046d89f85a9f977213da58bd
Matias Diaz <147676997+equinox-1092@users.noreply.github.com>
2025-05-14 09:05:54 -0500

    feat(services): add function create and url

---
+++ src/app/shared/services/base.service.ts
@@ -0,0 +1,36 @@
+import { environment } from '../../../environments/environment';
+import {HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {catchError, Observable, retry, throwError} from 'rxjs';
+
+export abstract class BaseService<T> {
+    protected httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    protected serverBaseUrl: string = `${environment.serverBaseUrl}`;
+    protected resourceEndpoint: string = '/resources';
+    protected http: HttpClient = inject(HttpClient);
+
+    getAll(): Observable<T[]> {
+        const url = `${this.serverBaseUrl}${this.resourceEndpoint}`;
+        return this.http.get<T[]>(url, this.httpOptions)
+            .pipe(
+                retry(2),
+                catchError((error: HttpErrorResponse) => {
+                    console.error('Error:', error);
+                    return throwError(() => new Error('Something bad happened; please try again later.'));
+                })
+            );
+    }
+
+    create(data: T): Observable<T> {
+        const url = `${this.serverBaseUrl}${this.resourceEndpoint}`;
+        return this.http.post<T>(url, data, this.httpOptions)
+            .pipe(
+                retry(2),
+                catchError((error: HttpErrorResponse) => {
+                    console.error('Error:', error);
+                    return throwError(() => new Error('Something bad happened; please try again later.'));
+                })
+            );
+    }
+}
