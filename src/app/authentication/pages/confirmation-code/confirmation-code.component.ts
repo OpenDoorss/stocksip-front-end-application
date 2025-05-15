4d7e20ab9bf558e826fb7267b72706b2aa55d22c
Camila Huamani <jyb990106@gmail.com>
2025-05-14 23:36:08 -0500

    feat(authentication): update confirmation code functions.

--- src/app/authentication/pages/confirmation-code/confirmation-code.component.ts
+++ src/app/authentication/pages/confirmation-code/confirmation-code.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -20,11 +21,12 @@ export class ConfirmationCodeComponent {
   code: string[] = ['', '', '', '', '', ''];
   codeArray = new Array(6);
 
+  constructor(private router: Router) {}
+
   autoFocusNext(event: any, index: number) {
     const input = event.target;
     let value = input.value.toUpperCase();
 
-    // Reemplaza cualquier carácter que no sea letra o número
     if (!/^[A-Z0-9]$/.test(value)) {
       input.value = '';
       this.code[index] = '';
@@ -39,4 +41,8 @@ export class ConfirmationCodeComponent {
     }
   }
 
+  goToLogin() {
+    console.log('Code:', this.code);
+    this.router.navigate(['/login']);
+  }
 }
