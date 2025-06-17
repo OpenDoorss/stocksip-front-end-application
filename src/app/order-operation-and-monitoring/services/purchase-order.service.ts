6cd332b93cbc0dd6fab9b07d3eee4509060481d2
Camila Huamani <jyb990106@gmail.com>
2025-06-16 22:42:40 -0500

    feat(orders): add purchase order entity.

---
+++ src/app/order-operation-and-monitoring/services/purchase-order.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { PurchaseOrder } from '../model/purchase-order.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PurchaseOrderService {
+  private apiUrl = `${environment.apiUrl}/purchase-orders`;
+
+  constructor(private http: HttpClient) {}
+
+  createPurchaseOrder(order: PurchaseOrder): Observable<any> {
+    return this.http.post(this.apiUrl, order);
+  }
+
+  getAll(): Observable<PurchaseOrder[]> {
+    return this.http.get<PurchaseOrder[]>(this.apiUrl);
+  }
+}
